bde3b80365479f26d4834309a09993dae32c8749
dahnte <cardenasdanten@gmail.com>
2023-09-12 17:32:02 -0700

    Added favicon, began layout phase

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<div class="container">
+    <div class="navbar">
+        <nav>
+            <a href="/">Home</a>
+            <a href="/blog">Blog</a>
+        </nav>
+    </div>
+
+    <div class="content">
+        <slot></slot>
+    </div>
+
+</div>
+
+<style>
+    .container {
+        display: flex;
+        justify-content: center;
+    }
+
+    .navbar a {
+        text-decoration: none;
+        display: block;
+        color: #000000;
+    }
+
+    .navbar a:hover {
+        background-color: #e0e0e0;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Needlessly Logged</h1>
+<h2><em>A skuapile blog</em></h2>
+
+<p>Living up to its name, this <em>blog</em> will serve to keep me in check and you possibly out of the loop.</p>
+<p>Basically, there's stuff I need to remeber and I find it convenient to access said stuff on all my devices.</p>
