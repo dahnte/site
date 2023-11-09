b37864571315e0e171938c844c64d7f0e2c33e2b
dahnte <70238020+dahnte@users.noreply.github.com>
2023-11-09 00:03:52 -0800

    Direct links

--- src/routes/liero/+page.svelte
+++ src/routes/liero/+page.svelte
@@ -1,5 +1,5 @@
 <h2><em>Liero</em></h2>
 <p>Just a direct link to mod files</p>
-<a href="./liero/mod.json5">mod.json5</a>
-<a href="./liero/sprites.wlsprt">sprites.wlsprt</a>
-<a href="./liero/ParasiticWorms.zip">ParasiticWorms.zip</a>
+<a href="../liero/mod.json5">mod.json5</a>
+<a href="../liero/sprites.wlsprt">sprites.wlsprt</a>
+<a href="../liero/ParasiticWorms.zip">ParasiticWorms.zip</a>
