7eea0bf4fcb3d09336a2d7ebecbda518ae98fbb4
dahnte <cardenasdanten@gmail.com>
2023-11-09 00:01:30 -0800

    Direct links

--- src/routes/liero/+page.svelte
+++ src/routes/liero/+page.svelte
@@ -1,9 +1,5 @@
-<script>
-	import { assets } from "$app/paths";
-
-</script>
 <h2><em>Liero</em></h2>
 <p>Just a direct link to mod files</p>
-<a href="%sveltekit.assets%/liero/mod.json5">mod.json5</a>
-<a href="%sveltekit.assets%/liero/sprites.wlsprt">sprites.wlsprt</a>
-<a href="%sveltekit.assets%/liero/ParasiticWorms.zip">ParasiticWorms.zip</a>
+<a href="./liero/mod.json5">mod.json5</a>
+<a href="./liero/sprites.wlsprt">sprites.wlsprt</a>
+<a href="./liero/ParasiticWorms.zip">ParasiticWorms.zip</a>
