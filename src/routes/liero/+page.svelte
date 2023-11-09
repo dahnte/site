704feeb2c7812c82861388860aad44870e0baa3a
dahnte <cardenasdanten@gmail.com>
2023-11-09 02:13:11 -0800

    Direct links

--- src/routes/liero/+page.svelte
+++ src/routes/liero/+page.svelte
@@ -2,4 +2,4 @@
 <p>Just a direct link to mod files</p>
 <a href="/site/liero/mod.json5">mod.json5</a>
 <a href="/site/liero/sprites.wlsprt">sprites.wlsprt</a>
-<a href="/site/liero/ParasiticWorms.zip">ParasiticWorms.zip</a>
+<a href="/site/liero/Parasitic_Worms.zip">Parasitic_Worms.zip</a>
